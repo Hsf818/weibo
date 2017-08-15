ee60f2b36882db2ff443448597c44dfacf97d38c
Hshenfa <836193660@qq.com>
2017-08-15 17:24:21 +0800

    初始化项目

---
+++ ShowGit/ShowGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShowGit
+//
+//  Created by hsf on 17/8/15.
+//  Copyright © 2017年 衡申发. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
